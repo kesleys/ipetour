1a7f3ffb94f5b78b7368d388eb08a6f4b53b9473
Kesley Soares <kesleysoares@discente.ufg.br>
2025-05-05 23:36:57 +0000

    correção de tipagem 2

--- ipetour/src/swiper.d.ts
+++ ipetour/src/swiper.d.ts
@@ -17,7 +17,3 @@ declare module "swiper/css/effect-fade" {
   const content: string;
   export default content;
 }
-declare module "flexDirection" {
-  const content: string;
-  export default content;
-}
