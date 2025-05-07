32872d328f293bc0c9275ba5b8814f6a26bb6f59
Kesley Soares <kesleysoares@discente.ufg.br>
2025-05-07 15:59:01 +0000

    Criar mapa usando iframe do google maps

---
+++ ipetour/src/components/contato_page/mapa_contato_page/mapa_contato_page.tsx
@@ -0,0 +1,14 @@
+import "./mapa_contato_page.css";
+
+export default function MapaContatoPage() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3822.356611902897!2d-49.33264041389827!3d-16.659028903851283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef5ad00f91ac9%3A0xcb454bbd74c52f23!2sPadre%20Pel%C3%A1gio!5e0!3m2!1spt-BR!2sbr!4v1746631378795!5m2!1spt-BR!2sbr"
+      width="600"
+      height="450"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+}
--- ipetour/src/pages/ContatoPage.tsx
+++ ipetour/src/pages/ContatoPage.tsx
@@ -1,12 +1,14 @@
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
-import { ContactForm } from "../components/contato/contato_formulario";
+import { ContactForm } from "../components/contato_page/contato_formulario_contato_page/contato_formulario";
+import MapaContatoPage from "../components/contato_page/mapa_contato_page/mapa_contato_page";
 
 export default function ContatoPage() {
   return (
     <div>
       <Header />
       <ContactForm />
+      <MapaContatoPage />
       <Footer />
     </div>
   );
