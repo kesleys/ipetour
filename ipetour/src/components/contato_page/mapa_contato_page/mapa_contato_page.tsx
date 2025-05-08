0d82aecd2e609030fa23249db36858f183c0dbc1
Kesley Soares <kesleysoares@discente.ufg.br>
2025-05-08 16:36:31 +0000

    alterar o endereço para o correto

--- ipetour/src/components/contato_page/mapa_contato_page/mapa_contato_page.tsx
+++ ipetour/src/components/contato_page/mapa_contato_page/mapa_contato_page.tsx
@@ -10,9 +10,7 @@ export default function MapaContatoPage() {
       <div className="limite_mapa_contato_page">
         <iframe
           className="mapa_contato_page"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3822.356611902897!2d-49.33264041389827!3d-16.659028903851283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef5ad00f91ac9%3A0xcb454bbd74c52f23!2sPadre%20Pel%C3%A1gio!5e0!3m2!1spt-BR!2sbr!4v1746631378795!5m2!1spt-BR!2sbr"
-          width="600"
-          height="450"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.8818087524764!2d-49.31666852390236!3d-16.682796845459887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef425fcb4e4ef%3A0xcd96c3683602535b!2sAv.%20Nero%20Macedo%2C%20400%20-%20Conj.%20Guadalajara%2C%20Goi%C3%A2nia%20-%20GO%2C%2074415-472!5e0!3m2!1spt-BR!2sbr!4v1746721964873!5m2!1spt-BR!2sbr"
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
